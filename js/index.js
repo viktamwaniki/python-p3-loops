c784d37a4352371675b77371127811dce53ac876
delamoar <you@example.com>
2023-08-17 11:51:03 +0300

    completed

--- js/index.js
+++ js/index.js
@@ -11,17 +11,7 @@ function happyNewYear() {
   console.log("Happy New Year!");
 }
 
-/* 
-  Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. For
-  multiples of three, print "Fizz" instead of the number and for the multiples
-  of five print "Buzz". For numbers which are multiples of both three and five,
-  print "FizzBuzz".
-*/
-function fizzbuzzPrinter() {
-  for (let num = 1; num <= 100; num++) {
-    console.log(fizzbuzz(num));
-  }
-}
+
 
 function fizzbuzz(num) {
   if (num % 3 === 0 && num % 5 === 0) {
@@ -35,18 +25,6 @@ function fizzbuzz(num) {
   }
 }
 
-/*
-  Write a method `reverse_string` that takes one argument, a string, and reverses
-  it. Don't use the built-in `.reverse` method. Instead, loop through the
-  characters in the input string and reverse it.
-*/
-function reverseString(str) {
-  let reversedStr = "";
-  for (let i = 0; i < str.length; i++) {
-    reversedStr = str[i] + reversedStr;
-  }
-  return reversedStr;
-}
 
 
 /*
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+def square_integers(input_list):
+    squared_list = []
+    
+    for num in input_list:
+        squared_list.append(num ** 2)
+    
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
+    
     pass
